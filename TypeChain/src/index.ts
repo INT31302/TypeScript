0bcd8e08f173ca226b1d3ef70278c257af95cfc3
INT31302 <tkdwo287@gmail.com>
2021-06-11 21:42:30 +0900

    #10

--- TypeChain/src/index.ts
+++ TypeChain/src/index.ts
@@ -75,6 +75,8 @@ const createNewBlock = (data: string): Block => {
 
 const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
   if (!Block.validateStructure(candidateBlock)) return false;
+  else if (previousBlock.index + 1 !== candidateBlock.index) return false;
+  else if (previousBlock.hash !== candidateBlock.previousHash) return false;
   else return true;
 };
 
